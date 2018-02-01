73bb2d02376fb18bc2b5001681dc374425847c6b
AlexRohde18 <32848255+AlexRohde18@users.noreply.github.com>
2018-02-01 12:36:56 -0500

    Upload

---
+++ 9-3 Sentence Capitalizer/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	    PROGRAM 9-3 SENTENCE CAPITALIZER
+    ====================================================================================================================
+    Write a method that accepts a String object as anargument and returns a copy of the string
+    with the first character of each sentence capitalized. For instance, if the argument is "hello.
+    my name is Joe. what is your name?" the method should return the string "Hello. My name
+    is Joe. What is your name?" Demonstrate the method in a program that asks the user to
+    input a string and then passes it to the method. The modified string should be displayed on
+    the screen.
+	 */
+    }
+}
---
+++ 9-5 Password Verifier/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	    PROGRAM 9-5 PASSWORD VERIFIER
+    ====================================================================================================================
+    Imagine you are developing a software package for Amazon.com that requires users to
+    enter their own passwords. Your software requires that users' passwords meet the
+    following criteria:
+        - The password should be at least six characters long.
+        - The password should contain at least one uppercase and at least one lowercase letter.
+        - The password should have at least one digit.
+    Write a class that verifies that a password meets the stated criteria. Demonstrate the class in
+    a program that allows the user to enter a password and then displays a message indicating
+    whether it is valid or not.
+	 */
+    }
+}
